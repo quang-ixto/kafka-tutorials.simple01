dc780a4419fa30f8056fbdfc768c7a7302ab387a
Quang Nhan Nguyen <qnguyen@ixto.de>
2021-05-20 16:55:01 +0200

    first commit

---
+++ src/main/java/kafkatutorials/producerconsumer/simple01/Simple01Application.java
@@ -0,0 +1,13 @@
+package kafkatutorials.producerconsumer.simple01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simple01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simple01Application.class, args);
+	}
+
+}
